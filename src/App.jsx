010194366d6e5a4acc2fa36df6e95bf53f3d9c2e
Alexandre Peyrichout <a.peyrichout@gmail.com>
2020-01-29 13:22:14 +0100

    added bootstrap

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
+import Context from "./context/";
+import LandingPage from "./pages/LandingPage";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
+import Profile from "./pages/Profile";
+import NotFound from "./pages/NotFound";
+
+const App = () => {
+  const [datas, setdatas] = useState();
+
+  return (
+    <div>
+      <Context.Provider
+        value={{
+          datas,
+          setdatas
+        }}
+      >
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={LandingPage} />
+            <Route path="/login" component={Login} />
+            <Route path="/signup" component={Signup} />
+            <Route path="/dashboard" component={Dashboard} />
+            <Route path="/profile" component={Profile} />
+            <Route path="/404" component={NotFound} />
+            <Redirect to="/404" />
+          </Switch>
+        </BrowserRouter>
+      </Context.Provider>
+    </div>
+  );
+};
+export default App;
